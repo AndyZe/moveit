3621bf1785a2d7af426e15c76ec03529bc52401d
AndyZe <andyz@utexas.edu>
2021-01-11 14:08:28 -0600

    Managers do not need to be mutable

--- moveit_core/collision_detection_bullet/include/moveit/collision_detection_bullet/collision_env_bullet.h
+++ moveit_core/collision_detection_bullet/include/moveit/collision_detection_bullet/collision_env_bullet.h
@@ -114,12 +114,12 @@ protected:
   void addLinkAsCollisionObject(const urdf::LinkSharedPtr& link);
 
   /** \brief Handles self collision checks */
-  mutable collision_detection_bullet::BulletDiscreteBVHManagerPtr manager_{
+  collision_detection_bullet::BulletDiscreteBVHManagerPtr manager_{
     new collision_detection_bullet::BulletDiscreteBVHManager()
   };
 
   /** \brief Handles continuous robot world collision checks */
-  mutable collision_detection_bullet::BulletCastBVHManagerPtr manager_CCD_{
+  collision_detection_bullet::BulletCastBVHManagerPtr manager_CCD_{
     new collision_detection_bullet::BulletCastBVHManager()
   };
 
